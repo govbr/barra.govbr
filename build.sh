2970ae544525c9bf18444f3ab2d8b30793202834
Nitai Silva <nitaibezerra@gmail.com>
2014-03-25 14:57:17 -0300

    Removi comando desnecess?rio de copia do arquivo final

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ echo ""
 if test -f "recipes/$1"
 then
     sh recipes/$1/compile.sh
-    cp recipes/$1/build/barra-brasil.js app/templates/$1/
 else
     echo "Error. Profile $1 does not exist."
 fi
