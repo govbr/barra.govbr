75eba8cdc9f5e6bcb7ddd9ed523d639a969b3644
carlos.vieira <caduvieira@users.noreply.github.com>
2014-04-07 15:17:33 -0300

    build.sh: Chamando o build para o default se nenhum profile ? especificado. barrabrasil.py: tratamento de SSL para testes. barra-copa.scss: modificado url da fonte para pegar http ou https conforme especificado pelo usu?rio

--- build.sh
+++ build.sh
@@ -7,11 +7,12 @@ else
     profile='default'
 fi
 
-if test -d "recipes/$1"
+if test -d "recipes/$profile"
 then
     cd recipes/$profile
     sh ./compile.sh
-    python ../../app/barrabrasil.py $profile
+    cd ../../app
+    python barrabrasil.py $profile
 else
     echo "Error. Profile $1 does not exist."
 fi
