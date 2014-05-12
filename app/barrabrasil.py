e81a913017ca597fb365f4d594807e6ea8e1d922
Carlos Vieira <caduvieira@users.noreply.github.com>
2014-05-12 16:09:31 -0300

    barrabrasil.py: colocando gzip para compressao da barra.

--- app/barrabrasil.py
+++ app/barrabrasil.py
@@ -1,6 +1,6 @@
 from flask import Flask, url_for, render_template, request, Response, make_response
 from config import Config
-import hashlib
+import hashlib, zlib
 
 # Criar Key e certificado
 # openssl genrsa 1024 > ssl.key
@@ -42,14 +42,19 @@ def barra():
     with app.open_resource('templates/%s/barra-brasil.js' % profile) as f:
         conteudo = f.read().decode('utf-8')
     etag = hashlib.sha1(conteudo.encode('utf-8')).hexdigest()
+    accept_encoding = request.headers.get('Accept-Encoding', '')
     if request.if_none_match and \
               etag in request.if_none_match:
         resposta = Response(status=304)
+	if accept_encoding and \
+		'gzip' in accept_encoding:
+	   conteudo = zlib.compress(conteudo.encode('utf8'))
+	   resposta.headers['Content-Encoding'] = 'gzip'
     else: # nao esta em cache do navegador
         resposta = make_response(conteudo)
         resposta.set_etag(etag)
     resposta.headers['Content-type'] = 'application/x-javascript'
-    resposta.headers['Cache-control'] = 'public, max-age: 86400' #24 horas
+    resposta.headers['Cache-control'] = 'public, max-age: 86401' #24 horas
     resposta.headers['Last-Modified'] = data
     return resposta
 
@@ -62,7 +67,7 @@ def fonte():
     conteudo = f.read().decode('base64')
     resposta = make_response(conteudo)
     resposta.headers['Content-type'] = 'application/x-font-woff'
-    resposta.headers['Cache-control'] = 'public, max-age: 86400' #24 horas
+    resposta.headers['Cache-control'] = 'public, max-age: 31536000' #1 ano
     resposta.headers['Last-Modified'] = data
     return resposta
 
