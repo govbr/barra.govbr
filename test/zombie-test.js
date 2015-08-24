45627d84829196f19275fb2303a1915adf7b17e5
Carlos Vieira <edu.carlos.vieira@gmail.com>
2015-08-24 13:06:31 -0300

    Atualizando pacotes npm para versões estaveis

--- test/zombie-test.js
+++ test/zombie-test.js
@@ -1,7 +1,7 @@
 var chai = require("chai"), plugin = require("chai-jq");
 chai.use(plugin);
 
-var expect = require("chai").expect, assert = require("chai").assert,
+var expect = require("chai").expect,
 	Browser = require("zombie"),
 	app = require("./app");
 
@@ -9,7 +9,7 @@ describe("Testes de conteúdo de HTML da barra", function() {
 	var server, browser, barraUrl;
 	barraUrl = "http://localhost:3000/";
 	before(function() {
-		server = app.listen(3000);
+		//server = app.listen(3000);
 	});
 
 	beforeEach(function() {
@@ -19,25 +19,21 @@ describe("Testes de conteúdo de HTML da barra", function() {
 
 	after(function() {
 		// depois dos testes fechar o servidor
-		server.close();
+		//server.close();
 	});
 
 	it("trocar o conteúdo do #barra-brasil pelo correto", function(done) {
 			browser.visit(barraUrl, function() {
-
          var inner_barra = browser.document.getElementById("barra-brasil");
-			expect(inner_barra.innerHTML).to.equal("<div id=\"wrapper-barra-brasil\"><div class=\"brasil-flag\"><a href=\"http://brasil.gov.br\" class=\"link-barra\">Brasil</a></div><span class=\"acesso-info\"><a href=\"http://brasil.gov.br/barra#acesso-informacao\" class=\"link-barra\">Acesso à informação</a></span><nav><ul class=\"list\"><li><a href=\"#\" id=\"menu-icon\"></a></li><li class=\"list-item first\"><a href=\"http://brasil.gov.br/barra#participe\" class=\"link-barra\">Participe</a></li><li class=\"list-item\"><a href=\"http://www.servicos.gov.br/?pk_campaign=barrabrasil\" class=\"link-barra\">Serviços</a></li><li class=\"list-item\"><a href=\"http://www.planalto.gov.br/legislacao\" class=\"link-barra\">Legislação</a></li><li class=\"list-item last last-item\"><a href=\"http://brasil.gov.br/barra#orgaos-atuacao-canais\" class=\"link-barra\">Canais</a></li></ul></nav></div>");
+			expect(inner_barra.innerHTML).to.equal("<div id=\"wrapper-barra-brasil\"><div class=\"brasil-flag\"><a href=\"http://brasil.gov.br\" class=\"link-barra\">Brasil</a></div><span class=\"acesso-info\"><a href=\"http://brasil.gov.br/barra#acesso-informacao\" class=\"link-barra\">Acesso à informação</a></span><nav><ul class=\"list\"><li><a href=\"#\" id=\"menu-icon\"></a></li><li class=\"list-item first\"><a href=\"http://brasil.gov.br/barra#participe\" class=\"link-barra\">Participe</a></li><li class=\"list-item\"><a href=\"http://www.servicos.gov.br/?pk_campaign=barrabrasil&amp;orgao=http://estruturaorganizacional.dados.gov.br/id/unidade-organizacional/2981\" class=\"link-barra\" id=\"barra-brasil-orgao\">Serviços</a></li><li class=\"list-item\"><a href=\"http://www.planalto.gov.br/legislacao\" class=\"link-barra\">Legislação</a></li><li class=\"list-item last last-item\"><a href=\"http://brasil.gov.br/barra#orgaos-atuacao-canais\" class=\"link-barra\">Canais</a></li></ul></nav></div>");
 			done();
 		});
    });
 	
    it("trocar o conteúdo do #footer-brasil pelo correto", function(done) {
-		browser.visit(barraUrl, function() {
-
+		   browser.visit(barraUrl, function() {
          var inner_footer = browser.document.getElementById("footer-brasil");
-			assert(inner_footer.innerHTML === "<div id=\"wrapper-footer-brasil\"><a href=\"http://www.acessoainformacao.gov.br/\"><span class=\"logo-acesso-footer\"></span></a><a href=\"http://www.brasil.gov.br/\"><span class=\"logo-brasil-footer\"></span></a></div>",
-				"Conteúdo do #footer-brasil deve ser o provido pela barra.js");
-
+			expect(inner_footer.innerHTML).to.equal("<div id=\"wrapper-footer-brasil\"><a href=\"http://www.acessoainformacao.gov.br/\"><span class=\"logo-acesso-footer\"></span></a><a href=\"http://www.brasil.gov.br/\"><span class=\"logo-brasil-footer\"></span></a></div>");
 			done();
 		});
 	});
