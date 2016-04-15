9772901fcfebc303d87c3be8c0f92a0ccee58bea
Carlos Vieira <edu.carlos.vieira@gmail.com>
2016-04-15 11:10:27 -0300

    Refactor nas traduções #22

--- recipes/default/compile.sh
+++ recipes/default/compile.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 export PATH=$PATH:../../node_modules/.bin/
+variable-replacer assets/conteudo-barra_i18n.html assets/conteudo-barra.html    --data=assets/i18n/traducao_pt_BR.json
+variable-replacer assets/conteudo-barra_i18n.html assets/conteudo-barra-fr.html --data=assets/i18n/traducao_fr.json
+variable-replacer assets/conteudo-barra_i18n.html assets/conteudo-barra-es.html --data=assets/i18n/traducao_es.json
+variable-replacer assets/conteudo-barra_i18n.html assets/conteudo-barra-en.html --data=assets/i18n/traducao_en.json
 ../../venv/bin/assetgen ./barra-brasil-1.yaml
 ../../venv/bin/assetgen ./barra-brasil-2.yaml
 ../../venv/bin/assetgen ./footer-brasil-1.yaml
